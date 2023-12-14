5c7b7cd38610c4becbe67cee611678fccbbb1fd8
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-12-14 19:53:04 +0700

    feat: update script

--- script.sh
+++ script.sh
@@ -8,3 +8,45 @@ docker container ls -a
 docker container start sample-volume
 
 docker container logs sample-volume
+
+docker container inspect sample-volume
+#c07a1f5b078c9ceba8c2a1c0123586a68cd782f17d8abed9714892742f326a7d
+
+docker container stop sample-volume
+
+#WORKING DIRECTORY
+docker build -t ahmadr0ni/sample-workdir -f Dockerfile .
+
+docker container create --name sample-workdir -p 8080:8080 ahmadr0ni/sample-workdir
+
+docker container ls -a
+
+docker container start sample-workdir
+
+docker container logs sample-workdir
+
+docker container inspect sample-workdir
+
+docker container exec -i -t sample-workdir /bin/sh
+
+#User Instruction
+#WORKING DIRECTORY
+docker build -t ahmadr0ni/sample-user -f Dockerfile .
+
+docker container create --name sample-user -p 8080:8080 ahmadr0ni/sample-user
+
+docker container ls -a
+
+docker container start sample-user
+
+docker container logs sample-user
+
+docker container inspect sample-user
+
+docker container exec -i -t sample-user /bin/sh
+
+#Argument instruction
+docker build -t ahmadr0ni/sample-user -f Dockerfile --build-arg app=bws .
+
+docker container create --name sample-arg -p 8080:8080 ahmadr0ni/sample-arg:latest
+
