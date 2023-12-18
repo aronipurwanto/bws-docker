e3352056846c1c2a25e7c926cbaef964da815f0d
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-12-18 18:01:46 +0700

    docker service

---
+++ belajar-docker-compose/script.sh
@@ -0,0 +1,11 @@
+#create image
+docker compose create
+
+docker compose start
+
+docker compose stop
+
+#kalau ingin satu per satu
+docker container start --container_name
+
+docker container stop --container_name
